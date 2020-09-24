42f2fa8c739c2e149048c8388a1ab714cd24a3fb
kromer02 <kromer02@email.franklin.edu>
2020-09-24 00:27:46 -0400

    added navigation between pages

--- cma/src/main/java/application/cma/controllers/HomeController.java
+++ cma/src/main/java/application/cma/controllers/HomeController.java
@@ -1,13 +1,35 @@
 package application.cma.controllers;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import application.cma.models.Client;
+import application.cma.repositories.ClientRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	ClientRepository clientRepo;
+	
 	@GetMapping(path="")
-	public String getHome() {
+	public String getHome(Model model) {
+	
+		int month = LocalDate.now().getMonthValue();
+		ArrayList<Client> clients = (ArrayList<Client>) clientRepo.findAll();		
+		ArrayList<Client> anniversaries = new ArrayList<Client>();
+		
+		for (Client client : clients) {
+			if (client.getDod().getMonthValue() == month) {
+				anniversaries.add(client);
+			}
+		}
+		model.addAttribute("anniversaries", anniversaries);
 		return "home";
 	}
 }
