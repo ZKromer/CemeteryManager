d78fd9e70d5db41fc7d511268d6b73932b728f08
kromer02 <kromer02@email.franklin.edu>
2020-09-24 00:00:03 -0400

    Added Death Anniversaries to home page

--- cma/src/main/java/application/cma/controllers/ClientController.java
+++ cma/src/main/java/application/cma/controllers/ClientController.java
@@ -5,10 +5,9 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import application.cma.models.Client;
@@ -26,12 +25,13 @@ public class ClientController {
 	}
 	
 	@PostMapping(path="clientEdit")
-	public String requestClientEdit(String firstName, String lastName, 
+	public String requestClientEdit(Model model, String firstName, String lastName, 
 			@RequestParam("dob") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dob,
 			@RequestParam("dod") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dod) {
 				
 		Client client = new Client(firstName, lastName, dob ,dod);
 		clientRepo.save(client);
-		return "clientEdit";
+		model.addAttribute("clients", clientRepo.findAll());
+		return "search";
 	}
 }
